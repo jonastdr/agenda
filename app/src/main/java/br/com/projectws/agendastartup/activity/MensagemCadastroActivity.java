29e58ca045bbdbf708cb9bda8b2671838561cc48
jonas <tortato.jonas@gmail.com>
2016-05-14 17:23:57 -0300

    ...

---
+++ app/src/main/java/br/com/projectws/agendastartup/activity/MensagemCadastroActivity.java
@@ -0,0 +1,15 @@
+package br.com.projectws.agendastartup.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.projectws.agendastartup.R;
+
+public class MensagemCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensagem_cadastro);
+    }
+}
