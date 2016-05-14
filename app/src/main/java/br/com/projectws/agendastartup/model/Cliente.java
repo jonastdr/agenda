a3f1768a4f386cd33df8509c6cb37abc024711bf
GuiAfonso <guilherminomarcos@gmail.com>
2016-05-14 16:41:51 -0300

    cleinte model

--- app/src/main/java/br/com/projectws/agendastartup/model/Cliente.java
+++ app/src/main/java/br/com/projectws/agendastartup/model/Cliente.java
@@ -4,4 +4,38 @@ package br.com.projectws.agendastartup.model;
  * Created by jonas on 14/05/16.
  */
 public class Cliente {
+    private String nome;
+    private String telefone, tags;
+
+    public Cliente() {}
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
 }
