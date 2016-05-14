051fc803bf5ed317217768d4c62f5fc8405ce0b9
jonas <tortato.jonas@gmail.com>
2016-05-14 18:55:43 -0300

    ...

--- app/src/main/java/br/com/projectws/agendastartup/activity/ClienteActivity.java
+++ app/src/main/java/br/com/projectws/agendastartup/activity/ClienteActivity.java
@@ -1,7 +1,13 @@
 package br.com.projectws.agendastartup.activity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.projectws.agendastartup.R;
 
@@ -11,5 +17,25 @@ public class ClienteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+
+        Button enviarBtn = (Button) findViewById(R.id.enviarBtn);
+
+        assert enviarBtn != null;
+        enviarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("mensagem", "minha mensagem..........");
+                clipboard.setPrimaryClip(clip);
+
+                Uri uri = Uri.parse("smsto:" + "+554299603082");
+
+                Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+
+                i.setPackage("com.whatsapp");
+
+                startActivity(i);
+            }
+        });
     }
 }
