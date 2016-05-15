d769b23a6f534c1444d4c9392dad267943127984
GuiAfonso <guilherminomarcos@gmail.com>
2016-05-15 06:56:45 -0300

    ....

--- app/src/main/java/br/com/projectws/agendastartup/activity/MainActivity.java
+++ app/src/main/java/br/com/projectws/agendastartup/activity/MainActivity.java
@@ -8,13 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,13 +16,7 @@ import br.com.projectws.agendastartup.R;
 import br.com.projectws.agendastartup.adapter.ClienteAdapter;
 import br.com.projectws.agendastartup.model.Cliente;
 import br.com.projectws.agendastartup.model.Mensagem;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private final OkHttpClient mClient = new OkHttpClient();
@@ -57,12 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-
-        try {
-            prepare();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -105,59 +87,5 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
-    }
-
-    private void prepare() throws Exception {
-
-        RequestBody requestBody = new FormBody.Builder()
-                .build();
-
-        Request request = new Request.Builder()
-                .url("http://192.168.0.15:8000/api/v1/contato/filter")
-                .post(requestBody)
-                .build();
-
-        mClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) throw new IOException("Erro de Conexão" + response);
-                try {
-                    JSONObject jsonResponse = new JSONObject(response.body().string());
-                    String status = jsonResponse.getString("status");
-                    if (new String("success").equals(status)) {
-                        clienteList = new ArrayList<>();
-                        JSONArray contato = jsonResponse.getJSONObject("options").getJSONArray("contato");
-                        for(int i = 0; i < contato.length(); i++) {
-                            JSONObject cont = (JSONObject) contato.get(i);
-                            JSONObject descricao = (JSONObject) cont.getJSONArray("perfil").get(0);
-                            System.out.println(descricao);
-                            cliente = new Cliente(
-                                    cont.getString("id"),
-                                    cont.getString("nome"),
-                                    cont.getString("numero"),
-                                    descricao.getString("descricao")
-                            );
-                            clienteList.add(cliente);
-                        }
-
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "Erro de Conexão", Toast.LENGTH_SHORT);
-                            }
-                        });
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 }
