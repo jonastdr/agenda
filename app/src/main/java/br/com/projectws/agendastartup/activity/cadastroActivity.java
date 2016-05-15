ce0023e3c56ed819492df2e5198cfe9ff6177ee4
GuiAfonso <guilherminomarcos@gmail.com>
2016-05-15 11:17:45 -0300

    ....

--- app/src/main/java/br/com/projectws/agendastartup/activity/cadastroActivity.java
+++ app/src/main/java/br/com/projectws/agendastartup/activity/cadastroActivity.java
@@ -1,6 +1,5 @@
 package br.com.projectws.agendastartup.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneNumberFormattingTextWatcher;
