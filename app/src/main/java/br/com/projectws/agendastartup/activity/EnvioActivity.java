bb13ff6f37381a569d7c786355760361f0f6808a
GuiAfonso <guilherminomarcos@gmail.com>
2016-05-15 02:21:44 -0300

    ....

--- app/src/main/java/br/com/projectws/agendastartup/activity/EnvioActivity.java
+++ app/src/main/java/br/com/projectws/agendastartup/activity/EnvioActivity.java
@@ -1,33 +1,29 @@
 package br.com.projectws.agendastartup.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 
 import br.com.projectws.agendastartup.R;
 import br.com.projectws.agendastartup.model.Cliente;
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class EnvioActivity extends AppCompatActivity {
-
-    TextView nomeTelefone, mensagem;
-
-    final OkHttpClient mClient = new OkHttpClient();
-
-    Cliente cliente;
+    private final OkHttpClient mClient = new OkHttpClient();
+    private TextView nomeTelefone, mensagem;
+    private Button envioBtn, cancelarBtn;
+    private Cliente cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +34,25 @@ public class EnvioActivity extends AppCompatActivity {
         mensagem = (TextView) findViewById(R.id.mensagemEditText);
 
         setView();
+        envioBtn = (Button) findViewById(R.id.enviarButton);
+        cancelarBtn = (Button) findViewById(R.id.cancelarButton);
 
-        Button envioBtn = (Button) findViewById(R.id.enviarButton);
-
-        assert envioBtn != null;
         envioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        cancelarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    finish();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -69,48 +75,23 @@ public class EnvioActivity extends AppCompatActivity {
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://192.168.0.15:8000/")
+                .url("http://192.168.0.15:8000")
                 .post(requestBody)
                 .build();
 
         mClient.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
             }
 
             @Override
-            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                if (!response.isSuccessful()) throw new IOException("Erro de Conexão" + response);
-                try {
-                    JSONObject jsonResponse = new JSONObject(response.body().string());
-                    String status = jsonResponse.getString("status");
-
-                    Log.d("resultado", jsonResponse.toString());
-
-                    System.out.println(status);
-                    if (new String("success").equals(status)) {
-                        finish();
-
-                        EnvioActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "Mensagem enviada.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        EnvioActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), "Falha no envio da mensagem.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                System.out.println(response.body().string());
             }
         });
+
+
     }
 }
